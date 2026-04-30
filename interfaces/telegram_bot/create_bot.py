fccebe91fa1e9d96ee9735dd0dd92c748710d049
viongaard <Alex3ind@gmail.com>
2026-04-30 13:47:41 +0300

    feat: add create_bot

--- interfaces/telegram_bot/create_bot.py
+++ interfaces/telegram_bot/create_bot.py
@@ -12,12 +12,13 @@ from db_handler.db_class import PostgresHandler
 pg_db = PostgresHandler(config('PG_LINK'))
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 
-# Настройка администраторов
-admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
-
 # Настройка логирования
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+# Загрузка переменных из .env
+ADMINS = [int(admin_id) for admin_id in config('TELEGRAM_BOT_ADMINS').split(',')]
+API_KEY = config('TELEGRAM_BOT_API_KEY')
 
 # Инициализация бота и диспетчера
 bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
